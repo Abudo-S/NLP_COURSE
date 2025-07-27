64ec856aefe09347cc9f090d4eab8ed82ac48816
Abudo-S <abudo.mohamed123456789@gmail.com>
2025-07-27 19:05:49 +0200

    initial commit

---
+++ data/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2a5c787c9e827678aa1844b028333b864683959de3b31105213b32dcc879c7bd
+size 566
---
+++ data/moviedata.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:656aadba3a765e534feebc799a2015e78236d447e29a5cf335d440f2c4f5d810
+size 3105
